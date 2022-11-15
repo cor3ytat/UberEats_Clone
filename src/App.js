eea9d13905aa849c40278f2fd56faa8f0d55d9d7
Corey Jones <cor3ytat@icloud.com>
2022-11-15 12:17:21 -0800

    All of my files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Category from "./components/Category";
+import Food from "./components/Food";
+import Footer from "./components/Footer";
+import HeadlineCards from "./components/HeadlineCards";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div >
+      <Navbar />
+      <Hero />
+      <HeadlineCards />
+      <Food />
+      <Category />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
